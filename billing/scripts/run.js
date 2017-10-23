792a8f67d57aea27b8c88d990c4bf4f2de70e364
HuJingwei <a479540195@gmail.com>
2017-10-23 13:43:10 +0800

    unlock 0

--- billing/scripts/run.js
+++ billing/scripts/run.js
@@ -8,6 +8,7 @@ const contractAddress = "0x832fc6b937d9b3f4617bc47ee5931161196c5825";
 const owner = "0x3ae88fe370c39384fc16da2c9e768cf5d2495b48";
 const beneficiary = "0xca9f427df31a1f5862968fad1fe98c0a9ee068c4";
 this.web3 =  new Web3(new Web3.providers.HttpProvider(endpoint));
+
 this.web3.personal.unlockAccount(account.address, account.password);
 const abiFile = fs.readFileSync('../build/contracts/DbotBilling.json');
 const attAbi = fs.readFileSync('../build/contracts/ATT.json');
@@ -19,7 +20,7 @@ const contract = this.web3.eth.contract(this.billContractsAbi);
 this.bill = contract.at(contractAddress);
 const attContract = this.web3.eth.contract(this.attContractsAbi);
 this.att = attContract.at(attAdd);
-// console.log(this.att);
+console.log(this.att);
 // this.att.generateTokens.sendTransaction(owner,33,{from:owner,gas:3000000});
 // this.att.approve.sendTransaction(contractAddress, 1000,{from:owner,gas:3000000});
 // console.log(this.att.allowance(owner,contractAddress,{from:owner,gas:3000000}))
