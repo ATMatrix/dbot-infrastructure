865fead44ab11a9c7ee31c24b7d7e22ca61d622c
HuJingwei <a479540195@gmail.com>
2017-10-23 18:40:18 +0800

    unit test

--- billing/migrations/2_deploy_contracts.js
+++ billing/migrations/2_deploy_contracts.js
@@ -14,12 +14,12 @@ var ERC20Token = artifacts.require("./att/ERC20Token.sol");
 var MiniMeToken = artifacts.require("./att/MiniMeToken.sol");
 
 
-const beneficiary = "0xca9f427df31a1f5862968fad1fe98c0a9ee068c4";
+const beneficiary = "0x3ae88fe370c39384fc16da2c9e768cf5d2495b48";
 const billingType = 1;
-const arg0 = 100;
+const arg0 = 99;
 const arg1 = 0;
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(MiniMeToken);
   deployer.deploy(ATT).then(function () {
     deployer.deploy(DbotBilling,ATT.address,beneficiary,billingType,arg0,arg1);
---
+++ billing/test/TestDbotBilling.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.11;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/billing/DbotBilling.sol";
+import "../contracts/charges/FreeCharge.sol";
+
+contract TestDbotBilling {
+
+
+}
