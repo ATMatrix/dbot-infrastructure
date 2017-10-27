076ce8cef3e9b59ccf144caa25399596a344786c
StormSpirit22 <icestormspirit@gmail.com>
2017-10-27 16:27:56 +0800

    update xiaoi dbot

--- worker/run.js
+++ worker/run.js
@@ -17,7 +17,8 @@
       web3.personal.unlockAccount(account.address, account.password);
       let provider = new Web3.providers.HttpProvider(endpoint);
       
-      const d = require('./baiduImageClassify')    
+      const d = require('./baiduImageClassify') 
+      const xiaoi = require('./xiaoi')   
       // let res = await d({method: 'animalDetect', url: 'http://t2.27270.com/uploads/tu/201612/357/7.png'});
       // const dataResult = JSON.stringify(res);
       // console.log("dataResult: ", dataResult);
@@ -33,47 +34,61 @@
       let eventFundsFrozen = businessToken.EventFundsFrozen();
       let eventFundsDeduct = businessToken.EventFundsDeduct();
       
-      const aiName = 'hhe';
+      let aiName = 'hhe';
+      let hex = '0x' + Buffer.from(aiName, 'utf8').toString("hex");
+      for(let i = hex.length; i < 66; i++) {
+          hex += '0';
+      }
+      aiName = hex;
+      console.log("aiName: ",aiName);
       const owner = contractAddress.from;
       const beneficiary = web3.eth.accounts[1];
   
       eventFundsFrozen.watch(function (err, res) {
         if (!err) {
-          let args = JSON.parse(res.args.arg);
+          console.log("res: ", res);
+          let args = res.args.arg;
           console.log(args)
+          // const aiId = JSON.parse(res.args._id);
            // console.log("res: ", res);
-           callId = parseInt(res.args._callID);
-           console.log("callId: ", callId);
-           
-           console.log("eventFundsFrozen has been detected ");
-  
-          try {
-            d(args).then((res) => {
-              console.log("res: ", res);
+           const aiId = res.args._id;
+           console.log("aiId: ", aiId);
+           if(aiId === aiName) {
+            callId = parseInt(res.args._callID);
+            console.log("callId: ", callId);
+            
+            console.log("eventFundsFrozen has been detected ");
+ 
+            try {
               //调用AI得到数据成功
-              if(res != null) {
-                const dataResult = JSON.stringify(res);
-                console.log("dataResult: ", dataResult);
-                //调用AI结束
-               businessToken.callFundsDeduct(aiName, callId, true, dataResult.toString(), {from: owner,gas: cost.gas}).then(async function(r){
-                await att.balanceOf(owner, {from:owner,gas:cost.gas}).then(function(r) {
-                  console.log("owner balance: ", r.toString());
+              xiaoi(args).then((res) => {
+               console.log("res: ", res);
+              //调用AI结束
+               if(res != null) {
+                 const dataResult = JSON.stringify(res);
+                 console.log("dataResult: ", dataResult);
+                 
+                businessToken.callFundsDeduct(aiName, callId, true, dataResult.toString(), {from: owner,gas: cost.gas}).then(async function(r){
+                 await att.balanceOf(owner, {from:owner,gas:cost.gas}).then(function(r) {
+                   console.log("owner balance: ", r.toString());
+                 });
+                 await att.balanceOf(beneficiary, {from:owner,gas:cost.gas}).then(function(r) {
+                   console.log("beneficiary balance:", r.toString());
+                 });
                 });
-                await att.balanceOf(beneficiary, {from:owner,gas:cost.gas}).then(function(r) {
-                  console.log("beneficiary balance:", r.toString());
-                });
-               });
-              }
-              else {
-               businessToken.callFundsDeduct(aiName, callId, false, dataResult.toString(), {from: owner,gas: cost.gas});
-              }
-            })
-          }
-          catch(e) {
-            console.log(e);
-          }
-          
-        }
+               }
+               else {
+                businessToken.callFundsDeduct(aiName, callId, false, dataResult.toString(), {from: owner,gas: cost.gas});
+               }
+             })
+           }
+           catch(e) {
+             console.log(e);
+           }
+           
+         }
+           }
+
     })
   }
     
