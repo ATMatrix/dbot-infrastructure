107a40a6fa040edfd4687d358c3d8d9d6c919a0c
StormSpirit22 <icestormspirit@gmail.com>
2017-12-11 13:02:13 +0800

    update xiaoi name

--- worker/xiaoi/run.js
+++ worker/xiaoi/run.js
@@ -1,5 +1,5 @@
 const monitor = require("../worker_factory")
 const config = require('../config.json').xiaoi
 
-let xiaoi = new monitor("xiaoi", config)
+let xiaoi = new monitor("XIAO_I", config)
 xiaoi.run();
