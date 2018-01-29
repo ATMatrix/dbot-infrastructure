e5e6a9595c8b005079ceffb76258f32f64abffcf
StormSpirit22 <icestormspirit@gmail.com>
2018-01-29 18:40:09 +0800

    update aliface name

--- worker/aliface/run.js
+++ worker/aliface/run.js
@@ -1,5 +1,5 @@
 const monitor = require("../worker_factory")
 const config = require('../config.json').aliface
 
-let aliface = new monitor("aliFace", config)
+let aliface = new monitor("aliface", config)
 aliface.run();
