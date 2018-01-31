40a08114fe2bef24242cf9facd72257b14a8f964
StormSpirit22 <icestormspirit@gmail.com>
2018-01-31 17:16:20 +0800

    update run.sh

--- worker/run.sh
+++ worker/run.sh
@@ -1,2 +1,8 @@
-
+forever start xiaoi/run.js
+forever start baiduOcr/run.js
+forever start aliface/run.js
+forever start baiduNlp/run.js
+forever start xunfei/run.js
+forever start azureVision/run.js
+forever start googleLanguage/run.js
 forever start ibmToneAnalyze/run.js
